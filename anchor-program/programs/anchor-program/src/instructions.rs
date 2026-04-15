21daf4019657ad3cdf6bfeaee5537f54be71662e
Sewang Rai <sewangsampang0.2@gmail.com>
2026-04-15 14:31:05 +0545

    feat: add instructions mod file with all five modules

--- anchor-program/programs/anchor-program/src/instructions.rs
+++ anchor-program/programs/anchor-program/src/instructions.rs
@@ -1,3 +1,11 @@
 pub mod initialize;
+pub mod register_policy;
+pub mod pay_premium;
+pub mod update_oracle;
+pub mod trigger_payout;
 
 pub use initialize::*;
+pub use register_policy::*;
+pub use pay_premium::*;
+pub use update_oracle::*;
+pub use trigger_payout::*;
